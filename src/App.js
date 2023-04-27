e141b9b74fab093b32f14c6ae3aa2e67e6c4f6af
Rangga <rangga@gurrly.com>
2023-04-27 10:44:26 +0800

    Refactor - Make category component reusable

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
 const App = () => {
+	const categories = [
+		{
+			id: 1,
+			title: "Hats",
+			image: "",
+		},
+		{
+			id: 2,
+			title: "Jackets",
+			image: "",
+		},
+		{
+			id: 3,
+			title: "Sneakers",
+			image: "",
+		},
+		{
+			id: 4,
+			title: "Womens",
+			image: "",
+		},
+		{
+			id: 5,
+			title: "Mens",
+			image: "",
+		},
+	];
+
 	return (
 		<div className="categories-container">
-			<div className="category-container">
-				{/* <img src="" alt="" /> */}
-				<div className="category-body-container">
-					<h2>Hats</h2>
-					<p>Shop Now</p>
+			{categories.map(({ title, id, image }) => (
+				<div className="category-container" key={id}>
+					<div className="background-image"></div>
+					<div className="category-body-container">
+						<h2>{title}</h2>
+						<p>Shop Now</p>
+					</div>
 				</div>
-			</div>
-			<div className="category-container">
-				{/* <img src="" alt="" /> */}
-				<div className="category-body-container">
-					<h2>Jackets</h2>
-					<p>Shop Now</p>
-				</div>
-			</div>
-			<div className="category-container">
-				{/* <img src="" alt="" /> */}
-				<div className="category-body-container">
-					<h2>Sneakers</h2>
-					<p>Shop Now</p>
-				</div>
-			</div>
-			<div className="category-container">
-				{/* <img src="" alt="" /> */}
-				<div className="category-body-container">
-					<h2>Womens</h2>
-					<p>Shop Now</p>
-				</div>
-			</div>
-			<div className="category-container">
-				{/* <img src="" alt="" /> */}
-				<div className="category-body-container">
-					<h2>Mens</h2>
-					<p>Shop Now</p>
-				</div>
-			</div>
+			))}
 		</div>
 	);
 };
