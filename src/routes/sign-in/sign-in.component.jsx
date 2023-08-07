80b04e7a72eefd97fe63c623b92652b805f27df8
Rangga <rangga@gurrly.com>
2023-08-07 19:56:21 +0800

    Setup sign in with email and password form

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { signInWithGooglePopup, createUserDocument } from "../../utils/firebase/firebase.utils";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
 	const logGoogleUser = async () => {
@@ -12,6 +13,8 @@ const SignIn = () => {
 		<div>
 			<h1>Sign in</h1>
 			<button onClick={logGoogleUser}>Sing in with Google</button>
+
+			<SignUpForm />
 		</div>
 	);
 };
